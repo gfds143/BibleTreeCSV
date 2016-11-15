56b8489a6a716ef9bcf681a0ce413ad27d6cd15f
gfds143 <aaron.holmes@eagles.oc.edu>
2016-11-15 17:44:03 -0600

    Added the BadgeSystemPeople.csv file and tested the file load to system models.

--- BadgeSystemPeopleUpload/Program.cs
+++ BadgeSystemPeopleUpload/Program.cs
@@ -1,49 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.Data.SqlClient;
-using Dapper;
+using BibleTree.Models;
 
 namespace BadgeSystemPeopleUpload
 {
     class Program
     {
-
+        static string[] adminUsers = { "Gage Ervin", "Aaron Holmes", "Tyler Kerr", "Ante Susic", "Chance Turner", "Andy Harbert" };
 
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Input file path.");
-            string filePath = Console.ReadLine();
+            //Console.WriteLine("Input file path.");
+            //string filePath = Console.ReadLine();
 
-            //List<Person> people = ReadFile(filePath);
+            string filePath = @"..\..\SourceFiles\BadgeSystemPeople.csv";
+
+            List<User> people = ReadFile(filePath);
+
+            List<Administrator> administrators = new List<Administrator>();
+            List<Student> students = new List<Student>();
+
+            DateTime enrollDate = new DateTime(2014, 8, 1);
+            DateTime graduateDate = new DateTime(2019, 4, 30);
+
+            foreach (User U in people)
+            {
+                if( IsIn(U.user_name, adminUsers)){
+
+                    Administrator tempAdmin = new Administrator();
+                    tempAdmin.mapUser(U);
+
+                    tempAdmin.user_type = 'a';
+                    tempAdmin.user_token = "Unused";
+
+                    tempAdmin.administrator_permLevel = 1;
+
+                    administrators.Add(tempAdmin);
+                }
+                else
+                {
+                    Student tempStu = new Student();
+                    tempStu.mapUser(U);
+
+                    tempStu.user_type = 's';
+                    tempStu.user_token = "Unused";
+
+                    //tempStu.student_id = students.Count;   ??? what is student ID and why is it a string
+                    tempStu.student_dateEnrolled = enrollDate;
+                    tempStu.student_dateGraduated = graduateDate;
+
+                    students.Add(tempStu);
+                }
+
+            }
 
             // insert the person into the database
         }
 
-        //static List<Person> ReadFile(string filePath)
-        //{
-        //    List<Person> people = new List<Person>();
+        static List<User> ReadFile(string filePath)
+        {
+            List<User> users = new List<User>();
 
-        //    System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+            System.IO.StreamReader file = new System.IO.StreamReader(filePath);
 
-        //    string line;
-        //    while((line = file.ReadLine()) != null)
-        //    {
-        //        string[] items = line.Split(',');
+            int count = 0;
+            string line;
 
-        //        Person temp = new Person(items[0], items[1]);
+            file.ReadLine();
 
-        //        people.Add(temp);
-        //    }
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(',');
 
-        //    file.Close();
+                //Person temp = new Person(items[0], items[1]);
 
-        //    return people;
-        //}
+                User temp = new User();
+                temp.user_name = items[0];
+                temp.user_email = items[1];
+                temp.user_id = count;
 
+                count++;
+
+                users.Add(temp);
+            }
+
+            file.Close();
+
+            return users;
+        }
+
+        static bool IsIn(string A, string[] B)
+        {
+            foreach (string S in B)
+            {
+                if (S == A)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
